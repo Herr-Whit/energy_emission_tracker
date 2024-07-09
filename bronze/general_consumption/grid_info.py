8ca86029385517a0ad8f92da57d4b27e66b96319
Herr-Whit <Herr-Whit>
2024-07-09 12:33:11 +0000

    inital grid info design

---
+++ bronze/general_consumption/grid_info.py
@@ -0,0 +1,39 @@
+import os
+import datetime
+import requests
+
+
+class GridInfoClient:
+    bnetz_url = "https://smard.api.bund.dev"
+
+    headers = {
+            'Content-Type': 'application/json',
+        }
+
+    def __init__(self):
+        pass
+
+    def get_indices(self, filter, region, resolution):
+        endpoint = f"/chart_data/{filter}/{region}/index_{resolution}.json"
+        return _fetch_from_api(endpoint)
+    
+
+    def get_indices(self, filter, region, resolution, timestamp):
+        endpoint = f"/chart_data/{filter}/{region}/{filter}_{region}_{resolution}_{timestamp}.json"
+        return _fetch_from_api(endpoint)
+        
+
+    def _fetch_from_api(self, endpoint):
+        
+        endpoint_url = self.bnetz_url + endpoint
+        # Send the request
+        response = requests.get(endpoint_url, headers=self.headers)
+        
+        # Check for errors
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Request failed with status code {response.status_code}")
+            print(response.text)
+            raise ValueError()
---
+++ bronze/general_consumption/ingest_global_production.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+from grid_info import GridInfoClient
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+client = GridInfoClient()
